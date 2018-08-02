14a1716ce7c2c866137ca4da245677cfa7a24c49
arushimathur05 <arushimathur05@gmail.com>
2018-08-01 20:47:56 -0700

    added keyboard input

---
+++ js/input.js
@@ -0,0 +1,18 @@
+
+function keyPressed() {
+    console.log("keyPressed keyCode = " + keyCode);
+
+    if (keyCode === LEFT_ARROW) {
+        game.player.c += -1;
+
+    } else if (keyCode === RIGHT_ARROW) {
+        game.player.c += 1;
+
+    } else if (keyCode === DOWN_ARROW) {
+        game.player.c += -1;
+
+    } else if (keyCode === UP_ARROW) {
+        game.player.c += -1;
+
+    }
+}
